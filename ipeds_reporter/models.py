629c1e27e63c243b26db23d8f188d25c57dfbcfd
crccheck <c@crccheck.com>
2014-08-12 15:24:20 -0500

    long category is long

--- ipeds_reporter/models.py
+++ ipeds_reporter/models.py
@@ -28,7 +28,7 @@ class Variable(models.Model):
     """An IPEDS report variable."""
     code = models.CharField(max_length=20)
     short_name = models.CharField(max_length=8)
-    category = models.CharField(max_length=150)
+    category = models.CharField(max_length=255)
     long_name = models.CharField(max_length=80)
     raw = models.CharField(max_length=800, unique=True)  # XXX not all database
                                                          # backends like this
