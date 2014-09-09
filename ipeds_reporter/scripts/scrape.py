3420dc09f59b5d82d632355824c68f0076bb14d2
crccheck <c@crccheck.com>
2014-09-09 12:21:01 -0500

    get to the 'select institutions' screen

--- ipeds_reporter/scripts/scrape.py
+++ ipeds_reporter/scripts/scrape.py
@@ -7,8 +7,20 @@ from selenium import webdriver
 
 
 def main():
-    driver = webdriver.Firefox()
-    driver.close()
+    print 'driver'
+    driver = webdriver.Chrome()
+    # driver = webdriver.Chrome('/path/to/chromedriver')
+    # driver = webdriver.Firefox()
+    print 'start'
+    # start session
+    driver.get('http://nces.ed.gov/ipeds/datacenter/')
+    # "Compare Institutions"
+    driver.find_element_by_id('tdInstData').click()
+    # > sent to http://nces.ed.gov/ipeds/datacenter/login.aspx
+    # TODO explicitly click "use final release data"
+    driver.find_element_by_id('ibtnLoginLevelOne').click()
+    # > sent to http://nces.ed.gov/ipeds/datacenter/InstitutionByName.aspx
+    # driver.close()
 
 
 if __name__ == '__main__':
